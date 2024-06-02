89e5b920dd536754b2e8b5b2f89e49efd1bdbb61
vanvi <kania.praca@wp.pl>
2024-06-02 16:50:33 +0200

    Solve Missing Integer

--- src/main/java/com/vankof/main/time_complexity/MissingInteger.java
+++ src/main/java/com/vankof/main/time_complexity/MissingInteger.java
@@ -1,6 +1,8 @@
 package com.vankof.main.time_complexity;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MissingInteger {
     public static void main(String[] args) {
@@ -11,9 +13,9 @@ public class MissingInteger {
         Arrays.sort(A);
         int smallestMissingInteger = 1;
 
-        for (final int i : A) {
-            if (i > smallestMissingInteger) {
-                if (i == smallestMissingInteger + 1) {
+        for (final int element : A) {
+            if (element > smallestMissingInteger) {
+                if (element == smallestMissingInteger + 1) {
                     smallestMissingInteger++;
                 } else {
                     return smallestMissingInteger;
@@ -21,5 +23,26 @@ public class MissingInteger {
             }
         }
         return smallestMissingInteger > 1 ? smallestMissingInteger + 1 : smallestMissingInteger;
+    }
+
+    /*
+        1. Load all data to HashSet
+        2. Increment smallesMissingPositive until it's not found in HashSet
+     */
+    public int solutionGpt(int[] A) {
+        final Set<Integer> positiveNumbers = new HashSet<>();
+
+        for (int num : A) {
+            if (num > 0) {
+                positiveNumbers.add(num);
+            }
+        }
+
+        int smallestMissingPositive = 1;
+        while (positiveNumbers.contains(smallestMissingPositive)) {
+            smallestMissingPositive++;
+        }
+
+        return smallestMissingPositive;
     }
 }
